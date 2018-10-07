0f8d50a5b4178896b6212117221ee1d82d74628d
Ronald Mansveld <ronald.mansveld@gmail.com>
2018-10-07 13:56:24 +0100

    Fix typo

--- src/models/eventId.ts
+++ src/models/eventId.ts
@@ -1,6 +1,6 @@
 export type CurrentEventId = '222' | '333' | '444' | '555' | '666' | '777' |  // cubes
                              '333bf' | '333fm' | '333oh' | '333ft' |          // 333 variations
-                             'clock' | 'minx' | 'pyra' | 'skewb' | 'sq1' |    // other puzzles
+                             'clock' | 'minx' | 'pyram' | 'skewb' | 'sq1' |    // other puzzles
                              '444bf' | '555bf' | '333mbf';                    // big and multiblind
 
 export type DeprecatedEventId = 'magic' | 'mmagic' | '333mbo';
