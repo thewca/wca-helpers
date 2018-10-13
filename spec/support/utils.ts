812ccc56d71e6e7ffcec4bc68bd18eefe793c6cf
Ronald Mansveld <ronald.mansveld@gmail.com>
2018-10-13 08:50:11 +0100

    Add wcaId tests

---
+++ spec/support/utils.ts
@@ -0,0 +1,5 @@
+export default function testCases(this: any, values: any[], func: (value: any) => void) {
+  for (let i = 0, count = values.length; i < count; i++) {
+      func.apply(this, [values[i]]);
+  }
+}
