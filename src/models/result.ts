64a042edc7e5f984de8bd5f6ca4b34830b7f1c8c
Caleb <choover11@gmail.com>
2022-05-11 14:56:54 -0700

    added missing properties to types (#12)

--- src/models/result.ts
+++ src/models/result.ts
@@ -1,7 +1,10 @@
 import { Attempt } from "./attempt";
+import { AttemptResult } from "./attemptResult";
 
 export interface Result {
   personId: number;
   ranking?: number;
   attempts: Attempt[];
+  best: AttemptResult;
+  average: AttemptResult;
 }
--- src/models/room.ts
+++ src/models/room.ts
@@ -1,8 +1,10 @@
 import { Activity } from "./activity";
+import { Extension } from "./extension";
 
 export interface Room {
   id: number;
   name: string;
   color: string;
   activities: Activity[];
+  extensions: Extension[];
 }
