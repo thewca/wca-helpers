8c1575f4f374aab4b94046a5808f502a0d3c3941
Choover <choover11@gmail.com>
2022-10-13 06:45:34 -0700

    Fixed tests regarding string multi blind result

--- src/helpers/result.ts
+++ src/helpers/result.ts
@@ -1,48 +1,54 @@
-import { AttemptResult } from "../models/attemptResult";
+import { AttemptResult } from '../models/attemptResult';
 import { formatCentiseconds } from './time';
 
 type DnfMultiResult = { isDnf: true };
 type DnsMultiResult = { isDns: true };
-type ValidMultiResult = { attempted: number, solved: number, centiseconds?: number };
+type ValidMultiResult = {
+  attempted: number;
+  solved: number;
+  centiseconds?: number;
+};
 
 type DecodedMultiResult = DnfMultiResult | DnsMultiResult | ValidMultiResult;
 
 export function isDnf(result: AttemptResult): boolean {
-  return `${result}` === '-1';
+  return result === -1;
 }
 
 export function isDns(result: AttemptResult): boolean {
-  return `${result}` === '-2';
+  return result === -2;
 }
 
 export function isMultiResult(result: AttemptResult): boolean {
-  return /^(0|1)\d{9}/.test(`${result}`);
-  // this is a save check, as the lowest MBLD formatted result would be 0010000000.
-  // this equates to 100000 seconds, which is nearly 28 hours
+  // Formatted as a base 10 string, a multi result will always have 8 or 9 digits.
+  // New style mbld has an implied 0 at the beginning which is not included in the length
+  return [9,10].indexOf(result.toString().length) > -1;
 }
 
 export function decodeMultiResult(result: AttemptResult): DecodedMultiResult {
-  let r = `${result}`;
-  if (r === '-1') {
+  let r = result;
+  if (r === -1) {
     return { isDnf: true };
   }
-  if (r === '-2') {
+  if (r === -2) {
     return { isDns: true };
   }
-  return r[0] === '1' ? decodeOldMultiResult(result) : decodeNewMultiResult(result);
+  return r.toString().length === 10 ? decodeOldMultiResult(result) : decodeNewMultiResult(result);
 }
 
-export function encodeMultiResult(result: DecodedMultiResult): string {
-  if (isDnfMultiResult(result)) return '-1';
-  if (isDnsMultiResult(result)) return '-2';
-  if (isMultiResultDnf(result)) return '-1';
+/**
+ * Encodes new style multiblind result
+ */
+export function encodeMultiResult(result: DecodedMultiResult): number {
+  if (isDnfMultiResult(result)) return -1;
+  if (isDnsMultiResult(result)) return -2;
+  if (isMultiResultDnf(result)) return -1;
 
   let missed = result.attempted - result.solved;
-  let dd = '' + (99 - (result.solved - missed));
-  let tt = '' + Math.round((result.centiseconds || 9999900) / 100); // multi results are always rounded to seconds, 99999 is used for 'unknown'
-  let mm = '' + missed;
+  let dd = 99 - (result.solved - missed);
+  let tt = Math.round((result.centiseconds || 9999900) / 100); // multi results are always rounded to seconds, 99999 is used for 'unknown'
 
-  return `0${padZero(dd, 2)}${padZero(tt, 5)}${padZero(mm, 2)}`;
+  return dd * 10000000 + tt * 100 + missed;
 }
 
 export function isMultiResultDnf(result: DecodedMultiResult): boolean {
@@ -63,24 +69,28 @@ export function formatMultiResult(result: DecodedMultiResult): string {
   let formatted = `${result.solved}/${result.attempted}`;
   if (result.centiseconds && result.centiseconds !== 9999900) {
     let fcs = formatCentiseconds(result.centiseconds);
-    formatted += ` ${ fcs.substring(0, fcs.length - 3) }`; // chop off the decimal part
+    formatted += ` ${fcs.substring(0, fcs.length - 3)}`; // chop off the decimal part
   }
   return formatted;
 }
 
-function isDnfMultiResult(result: DecodedMultiResult): result is DnfMultiResult {
+function isDnfMultiResult(
+  result: DecodedMultiResult
+): result is DnfMultiResult {
   return result.hasOwnProperty('isDnf');
 }
 
-function isDnsMultiResult(result: DecodedMultiResult): result is DnsMultiResult {
+function isDnsMultiResult(
+  result: DecodedMultiResult
+): result is DnsMultiResult {
   return result.hasOwnProperty('isDns');
 }
 
 function decodeOldMultiResult(result: AttemptResult): DecodedMultiResult {
-  let r = `${result}`;
-  let ss = parseInt(r.substr(1, 2), 10);
-  let aa = parseInt(r.substr(3, 2), 10);
-  let tt = parseInt(r.substr(5, 5), 10);
+  let r = result.toString();
+  let ss = parseInt(r.substring(1, 3), 10);
+  let aa = parseInt(r.substring(3, 5), 10);
+  let tt = parseInt(r.substring(5, 10), 10);
 
   let res: ValidMultiResult = { attempted: aa, solved: 99 - ss };
   if (tt !== 99999) {
@@ -90,10 +100,10 @@ function decodeOldMultiResult(result: AttemptResult): DecodedMultiResult {
 }
 
 function decodeNewMultiResult(result: AttemptResult): DecodedMultiResult {
-  let r = `${result}`;
-  let dd = parseInt(r.substr(1, 2), 10);
-  let tt = parseInt(r.substr(3, 5), 10);
-  let mm = parseInt(r.substr(8, 2), 10);
+  let r = result.toString();
+  let dd = parseInt(r.substring(0, 2), 10);
+  let tt = parseInt(r.substring(2, 7), 10);
+  let mm = parseInt(r.substring(7, 9), 10);
   let difference = 99 - dd;
   let solved = difference + mm;
   let attempted = solved + mm;
@@ -103,12 +113,4 @@ function decodeNewMultiResult(result: AttemptResult): DecodedMultiResult {
     res.centiseconds = tt * 100;
   }
   return res;
-}
-
-function padZero(str: string, length: number): string {
-  let s = str;
-  for (let i = 0; i < length; i++) {
-    s = '0' + s;
-  }
-  return s.substr(-1 * length, length);
 }
