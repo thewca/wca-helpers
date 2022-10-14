6ab4fda4d63b33c1f28ea6a00893ae3cc310382c
Caleb <choover11@gmail.com>
2022-10-14 05:47:58 -0700

    Deprecate the feet event (#17)

--- src/models/eventId.ts
+++ src/models/eventId.ts
@@ -1,8 +1,8 @@
 export type CurrentEventId = '222' | '333' | '444' | '555' | '666' | '777' |  // cubes
-                             '333bf' | '333fm' | '333oh' | '333ft' |          // 333 variations
-                             'clock' | 'minx' | 'pyram' | 'skewb' | 'sq1' |    // other puzzles
+                             '333bf' | '333fm' | '333oh' |                    // 333 variations
+                             'clock' | 'minx' | 'pyram' | 'skewb' | 'sq1' |   // other puzzles
                              '444bf' | '555bf' | '333mbf';                    // big and multiblind
 
-export type DeprecatedEventId = 'magic' | 'mmagic' | '333mbo';
+export type DeprecatedEventId = 'magic' | 'mmagic' | '333mbo' | '333ft';
 
 export type EventId = CurrentEventId | DeprecatedEventId;
